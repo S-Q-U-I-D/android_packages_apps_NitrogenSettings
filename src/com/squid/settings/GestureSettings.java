edb4156830250307df94b66e85040995fc7a1c4a
maxwen <max.weninger@gmail.com>
2018-08-20 23:46:00 +0000

    SquidSettings: implement OP like navigation gestures [2/2]
    Signed-off-by: Vol Zhdanov <wight554@gmail.com>

---
+++ src/com/squid/settings/GestureSettings.java
@@ -0,0 +1,89 @@
+
+package com.squid.settings;
+
+ import com.android.internal.logging.nano.MetricsProto;
+
+ import android.content.Context;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.provider.SearchIndexableResource;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
+
+ import com.android.settings.R;
+
+ import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+
+ import java.util.ArrayList;
+import java.util.List;
+
+ public class NosGestureSettings extends SettingsPreferenceFragment implements
+                          Preference.OnPreferenceChangeListener, Indexable {
+
+     private static final String KEY_TORCH_LONG_PRESS_POWER_TIMEOUT =
+            "torch_long_press_power_timeout";
+
+     private ListPreference mTorchLongPressPowerTimeout;
+
+     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+         addPreferencesFromResource(R.xml.squid_settings_gestures);
+
+         mTorchLongPressPowerTimeout =
+                    (ListPreference) findPreference(KEY_TORCH_LONG_PRESS_POWER_TIMEOUT);
+
+         mTorchLongPressPowerTimeout.setOnPreferenceChangeListener(this);
+        int TorchTimeout = Settings.System.getInt(getContentResolver(),
+                        Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, 0);
+        mTorchLongPressPowerTimeout.setValue(Integer.toString(TorchTimeout));
+        mTorchLongPressPowerTimeout.setSummary(mTorchLongPressPowerTimeout.getEntry());
+    }
+
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (preference == mTorchLongPressPowerTimeout) {
+            String TorchTimeout = (String) newValue;
+            int TorchTimeoutValue = Integer.parseInt(TorchTimeout);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, TorchTimeoutValue);
+            int TorchTimeoutIndex = mTorchLongPressPowerTimeout
+                    .findIndexOfValue(TorchTimeout);
+            mTorchLongPressPowerTimeout
+                    .setSummary(mTorchLongPressPowerTimeout.getEntries()[TorchTimeoutIndex]);
+            return true;
+        }
+        return false;
+    }
+
+     @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SQUID;
+    }
+
+     /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                 @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+
+                     final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.extra_settings_gestures;
+                    result.add(sir);
+                    return result;
+                }
+
+                 @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
+}
